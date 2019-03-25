658d1d7f8e9914c325564d541338b6dc69b4f861
MoeSamee <47723277+MoeSamee@users.noreply.github.com>
2019-03-25 17:58:41 -0400

    Update Navigation.java

--- src/ca/mcgill/ecse211/model/Navigation.java
+++ src/ca/mcgill/ecse211/model/Navigation.java
@@ -1,7 +1,6 @@
 package ca.mcgill.ecse211.model;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import ca.mcgill.ecse211.model.*;
 
 /**
  * This class contains all the methods that contribute to making
@@ -12,12 +11,14 @@ import ca.mcgill.ecse211.model.*;
  */
 public class Navigation {
 
-  public final static int SLOW_SPEED = 75;
   public final static int ROTATION_SPEED = 100;
-  private final static int FORWARD_SPEED = 200; 
+  private static final int SLOW_SPEED = 75;
+  
+  private final static int FORWARD_SPEED = 200;
+  private final static int FORWARD_SPEED_WEIGHT = 200;
   
   public static final double TILE_SIZE = 30.48;
-  
+ 
   private final double RADIUS = Robot.WHEEL_RAD;
   private final double TRACK = Robot.TRACK;
   
@@ -27,7 +28,7 @@ public class Navigation {
   public static double minAng;
 
   /**
-   * This is the default constructor of this class
+   * This is the default constructor of this class.
    * @param leftMotor left motor of robot
    * @param rightMotor right motor of robot
    * @throws OdometerExceptions
@@ -39,7 +40,7 @@ public class Navigation {
 	}
   
   /**
-   * Method that allows robot to travel from its current position to any other point
+   * Method that allows robot to travel from its current position to any other point.
    * @param x x coordinate of desired destination
    * @param y y coordinate of desired destination
    */
@@ -74,33 +75,18 @@ public class Navigation {
   }
 
   /**
-   * Method that allows the robot to rotate in place 
+   * Method that allows the robot to rotate in place.
    * @param theta angle amount (in degrees) that you want the robot to rotate in place
    */
   public void turnTo(double theta) {
 	leftMotor.setSpeed(ROTATION_SPEED);
 	rightMotor.setSpeed(ROTATION_SPEED);
 	leftMotor.rotate(convertAngle(RADIUS, TRACK, theta), true);
-	rightMotor.rotate(-convertAngle(RADIUS, TRACK, theta), false);
-
+    rightMotor.rotate(-convertAngle(RADIUS, TRACK, theta), false);
   }
   
   /**
-   * This method is only used when scanning for a can in the search zone. It is
-   * similar to turnTo(), but the motors rotate at a slower speed, and return
-   * instantly rather than wait for the robot to rotate.
-   * @param theta angle amount (in degrees) that you want the robot to rotate in place
-   */
-  public void turnToScan(double theta) {
-		leftMotor.setSpeed(SLOW_SPEED);
-		rightMotor.setSpeed(SLOW_SPEED);
-		leftMotor.rotate(convertAngle(RADIUS, TRACK, theta), true);
-		rightMotor.rotate(-convertAngle(RADIUS, TRACK, theta), true);
-
-	  }
-  
-  /**
-   * Method that allows the robot to move backward
+   * Method that allows the robot to move backward.
    * @param distance distance that you want the robot to travel backward
    */
   public void driveBack(double distance) {
@@ -112,7 +98,7 @@ public class Navigation {
   }
   
   /**
-   * Method that allows the robot to move forward
+   * Method that allows the robot to move forward.
    * @param distance distance that you want the robot to travel forward
    */
   public void driveForward(double distance) {
@@ -123,29 +109,58 @@ public class Navigation {
 	  rightMotor.rotate(convertDistance(RADIUS, distance), false);
   }
   
+  /**
+   * Method that allows the robot to move forward and allows other code to
+   * run
+   * @param distance distance that you want the robot to travel forward
+   */
+  public void driveForwardWeight(double distance) {
+	  leftMotor.setSpeed(FORWARD_SPEED_WEIGHT);
+	  rightMotor.setSpeed(FORWARD_SPEED_WEIGHT);
+
+	  leftMotor.rotate(convertDistance(RADIUS, distance), true);
+	  rightMotor.rotate(convertDistance(RADIUS, distance), true);
+  }
+  
+  /**
+   * This method is only used when scanning for a can in the search zone. It is
+   * similar to turnTo(), but the motors rotate at a slower speed, and return
+   * instantly rather than wait for the robot to rotate.
+   * @param theta angle amount (in degrees) that you want the robot to rotate in place
+   */
+  public void turnToScan(double theta) {
+        leftMotor.setSpeed(SLOW_SPEED);
+        rightMotor.setSpeed(SLOW_SPEED);
+        leftMotor.rotate(convertAngle(RADIUS, TRACK, theta), true);
+        rightMotor.rotate(-convertAngle(RADIUS, TRACK, theta), true);
+
+
+      }
+  
+
 	/**
 	 * This method converts a distance into the total rotation (in degrees) of 
 	 * each wheel needed to cover that distance 
 	 * 
-	 * @param RADIUS RADIUS of wheel
+	 * @param RADIUS radius of wheel
 	 * @param distance distance that you want the robot to move
 	 * @return total rotation (in degrees) of each wheel needed to cover a distance
 	 */
-	public static int convertDistance(double RADIUS, double distance) {
-	  return (int) ((180.0 * distance) / (Math.PI * RADIUS));
+	public static int convertDistance(double radius, double distance) {
+	  return (int) ((180.0 * distance) / (Math.PI * radius));
 	}
 	
 	/**
 	 * This method converts a rotation in place into the total 
 	 * rotation (in degrees) of each wheel needed to cause that rotation
 	 * 
-	 * @param RADIUS RADIUS of wheel
+	 * @param radius radius of wheel
 	 * @param width distance between centers of wheels
 	 * @param angle angle (in place) that you want the robot to rotate
 	 * @return total rotation (in degrees) of each wheel needed to cause a rotation in place
 	 */
-	public static int convertAngle(double RADIUS, double width, double angle) {
-	  return convertDistance(RADIUS, Math.PI * width * angle / 360.0);
+	public static int convertAngle(double radius, double width, double angle) {
+	  return convertDistance(radius, Math.PI * width * angle / 360.0);
 	}  
   
 }
